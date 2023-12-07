033d32e7c56358bb4350044e47152c511015f48c
Frédéric Thierrée <fredericthierree@gmail.com>
2023-12-07 12:42:09 +0100

    Cleaning out code

--- face_restoration_worker/utils/basicsr/__init__.py
+++ face_restoration_worker/utils/basicsr/__init__.py
@@ -1,8 +1,6 @@
 # https://github.com/xinntao/BasicSR
 # flake8: noqa
 from .archs import *
-from .data import *
-from .losses import *
 from .models import *
 from .ops import *
 from .utils import *
