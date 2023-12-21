7d6871eed220da40bbd304d7f721238f4c43c168
Frédéric Thierrée <fredericthierree@gmail.com>
2023-12-21 13:15:04 +0100

    Adding models download script

---
+++ download_models.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth -O ./models/codeformer.pth
+wget https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/RealESRGAN_x2plus.pth -O ./models/RealESRGAN_x2plus.pth
+wget https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/parsing_parsenet.pth -O ./models/parsing_parsenet.pth
+wget https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/detection_Resnet50_Final.pth -O ./models/detection_Resnet50_Final.pth
