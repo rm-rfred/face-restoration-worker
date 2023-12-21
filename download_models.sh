84a6f70d73d6230f4a91574cfef25bfd388d36f5
Frédéric Thierrée <fredericthierree@gmail.com>
2023-12-21 13:18:10 +0100

    Crating models directory when it doesn't exist

--- download_models.sh
+++ download_models.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir models
 wget https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth -O ./models/codeformer.pth
 wget https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/RealESRGAN_x2plus.pth -O ./models/RealESRGAN_x2plus.pth
 wget https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/parsing_parsenet.pth -O ./models/parsing_parsenet.pth
