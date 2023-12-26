73345fd267c553108747d44b72ffa8de3c955f9e
Frédéric Thierrée <fredericthierree@gmail.com>
2023-12-26 13:25:16 +0100

    Update run_protoc with project architecture

--- run_protoc.sh
+++ run_protoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x # Echo on
-python3.10 -m grpc_tools.protoc --proto_path=./face_restoration_worker_client/config --python_out=./face_restoration_worker_client/config --grpc_python_out=./face_restoration_worker_client/config ./face_restoration_worker_client/config/face_restoration.proto
+python3.10 -m grpc_tools.protoc --proto_path=./face_restoration_worker_client/grpc_config --python_out=./face_restoration_worker_client/grpc_config --grpc_python_out=./face_restoration_worker_client/grpc_config ./face_restoration_worker_client/grpc_config/face_restoration.proto
